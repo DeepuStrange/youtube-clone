d823ba42c9bc9348ecc5bcc052513f06d4b25817
pradeep <pradeep9100234360@gmail.com>
2023-10-25 20:36:24 +0530

    replaced displaying video

--- src/containers/Display.js
+++ src/containers/Display.js
@@ -4,7 +4,6 @@ function Display(){
     // console.log(selectedVideo,"SVV")
     if(selectedVideo.length > 0){
         const URL = `https://www.youtube.com/embed/${selectedVideo[0].id.videoId}`;
-        {/* <iframe width="560" height="315" src="https://www.youtube.com/embed/ACeWtcxJ2rk?si=GY1S1Fj4MINfkoa4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> */}
     return(
         <div>
             <iframe width="560" height="315" src={URL} ></iframe>
